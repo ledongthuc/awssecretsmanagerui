102b62cc1f2af471e2b5d57925828f0649dcf85b
Thinh Le <lqthinh07@gmail.com>
2021-07-15 22:02:18 +0700

    Configure style for MoaiJs component

--- ui-react/pages/index.tsx
+++ ui-react/pages/index.tsx
@@ -4,8 +4,10 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.content} style={{ background: "yellow" }}>
-        <Button highlight style={Button.styles.flat}>Log In</Button>
+      <div className={styles.content}>
+        <div className={styles.form}>
+          <Button highlight fill href="/list">Login</Button>
+        </div>
       </div>
     </div>
   )
