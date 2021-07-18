da0f14cceb902ed3d73b4127a78803e0569c298b
Thinh Le <lqthinh07@gmail.com>
2021-07-18 17:44:46 +0700

    Feat: Create components for list page

--- ui-react/pages/list.tsx
+++ ui-react/pages/list.tsx
@@ -1,11 +1,16 @@
 import Layout from "../components/Layout"
+import SearchBar from "../components/SearchBar";
+import Table from "../components/Table";
+import Pagination from "../components/Pagination";
 import styles from "../styles/List.module.css";
 
 export default function Home() {
   return (
     <Layout title="AWS Secrets Manager">
       <div className={styles.container}>
-        Secret List
+        <SearchBar />
+        <Table />
+        <Pagination />
       </div>
     </Layout>
   )
